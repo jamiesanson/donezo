5e0165761f026d5836a1a473132e9036e24dad26
Jamie Sanson <jamie@sanson.dev>
2021-02-13 11:04:27 +1300

    Initial commit

---
+++ app/src/main/java/dev/sanson/tick/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.sanson.tick
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+import dev.sanson.tick.ui.theme.TickTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TickTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TickTheme {
+        Greeting("Android")
+    }
+}
