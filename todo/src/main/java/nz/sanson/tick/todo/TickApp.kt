0e2261190134575a5005600312d41cd1bb0e1ba8
Jamie Sanson <jamie.sanson@trademe.co.nz>
2021-02-18 08:59:16 +1300

    Adds sqldelight

--- todo/src/main/java/nz/sanson/tick/todo/TickApp.kt
+++ todo/src/main/java/nz/sanson/tick/todo/TickApp.kt
@@ -1,5 +1,7 @@
 package nz.sanson.tick.todo
 
+import android.content.Context
+import com.squareup.sqldelight.android.AndroidSqliteDriver
 import nz.sanson.tick.todo.feature.navigation.navigationReducer
 import org.reduxkotlin.Store
 import org.reduxkotlin.combineReducers
@@ -9,7 +11,15 @@ import org.reduxkotlin.createThreadSafeStore
  * [createApp] wires up all the necessary Redux components, returning the [Store] to the consuming
  * frontend.
  */
-fun createApp(): Store<AppState> {
+fun createApp(context: Context): Store<AppState> {
+    val database = initialiseDb(context)
+
     val reducer = combineReducers(navigationReducer, rootReducer)
+
     return createThreadSafeStore(reducer, AppState())
+}
+
+private fun initialiseDb(context: Context): Database {
+    val driver = AndroidSqliteDriver(Database.Schema, context, "todo.db")
+    return Database(driver)
 }
