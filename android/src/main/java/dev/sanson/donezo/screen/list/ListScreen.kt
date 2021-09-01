214423ddfc9de65ca311dd62195bf3ae32d0ac23
Jamie Sanson <jamie@sanson.dev>
2021-09-01 20:53:22 +1200

    Remove unused imports in ListScreen.kt

--- android/src/main/java/dev/sanson/donezo/screen/list/ListScreen.kt
+++ android/src/main/java/dev/sanson/donezo/screen/list/ListScreen.kt
@@ -1,6 +1,5 @@
 package dev.sanson.donezo.screen.list
 
-import android.util.Log
 import androidx.compose.animation.core.Spring.StiffnessHigh
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.gestures.animateScrollBy
@@ -9,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -18,12 +16,10 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.layout.RelocationRequester
 import androidx.compose.ui.layout.relocationRequester
@@ -36,10 +32,6 @@ import dev.sanson.donezo.model.Todo
 import dev.sanson.donezo.model.TodoList
 import dev.sanson.donezo.theme.DonezoTheme
 import dev.sanson.donezo.todo.Action
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @Composable
 fun ListScreen(lists: List<TodoList>, dispatch: (Any) -> Any = LocalDispatch.current) {
