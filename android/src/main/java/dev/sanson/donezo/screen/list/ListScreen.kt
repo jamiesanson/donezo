b86c2fd45a7b41fa6b85e2892407bb94177c3687
Jamie Sanson <jamie@sanson.dev>
2021-09-03 17:28:05 +1200

    Minimise composition passes to converge when navigating with keyboard

--- android/src/main/java/dev/sanson/donezo/screen/list/ListScreen.kt
+++ android/src/main/java/dev/sanson/donezo/screen/list/ListScreen.kt
@@ -1,9 +1,6 @@
 package dev.sanson.donezo.screen.list
 
-import androidx.compose.animation.core.Spring.StiffnessHigh
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.ScrollState
-import androidx.compose.foundation.gestures.animateScrollBy
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -12,7 +9,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,83 +17,86 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.layout.RelocationRequester
 import androidx.compose.ui.layout.relocationRequester
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
 import dev.sanson.donezo.android.LocalDispatch
 import dev.sanson.donezo.model.Todo
 import dev.sanson.donezo.model.TodoList
 import dev.sanson.donezo.theme.DonezoTheme
 import dev.sanson.donezo.todo.Action
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @Composable
 fun ListScreen(lists: List<TodoList>, dispatch: (Any) -> Any = LocalDispatch.current) {
     val focusManager = LocalFocusManager.current
-    val density = LocalDensity.current
 
-    var focusDirectionToMove by remember { mutableStateOf<FocusDirection?>(null) }
+    var previousAction by remember { mutableStateOf<Any?>(null) }
 
     val wrappedDispatch: (Any) -> Any = { action ->
-        when (action) {
-            is Action.AddTodo, is Action.AddTodoAfter -> focusDirectionToMove = FocusDirection.Down
-            is Action.DeleteTodo, is Action.DeleteList -> focusDirectionToMove = FocusDirection.Up
+        dispatch(action).also {
+            previousAction = action
         }
-
-        // Move up immediately, as the composable above the current one probably exists
-        if (focusDirectionToMove == FocusDirection.Up) {
-            focusManager.moveFocus(FocusDirection.Up)
-            focusDirectionToMove = null
-        }
-
-        dispatch(action)
     }
 
-    val scrollState = rememberScrollState()
+    TodoListColumn(lists, wrappedDispatch)
 
-    TodoListColumn(scrollState, lists, wrappedDispatch)
+    SideEffect {
+        val action = previousAction ?: return@SideEffect
 
-    val itemCount = derivedStateOf { lists.fold(0) { acc, list -> acc + 1 + list.items.size } }
-    LaunchedEffect(itemCount) {
-        val focusDirection = focusDirectionToMove
-
-        if (focusDirection != null) {
-            val itemHeightPx = density.run { 52.dp.toPx() }
-            scrollState.animateScrollBy(
-                value = if (focusDirection == FocusDirection.Down) itemHeightPx else -itemHeightPx,
-                animationSpec = spring(stiffness = StiffnessHigh)
-            )
-
-            focusManager.moveFocus(focusDirection = focusDirection)
+        val focusDirection = when {
+            action is Action.DeleteList -> FocusDirection.Up
+            action is Action.DeleteTodo && lists.find { it.items.contains(action.item) }?.items?.size ?: 0 > 1 -> FocusDirection.Up
+            else -> null
         }
 
-        focusDirectionToMove = null
+        if (focusDirection != null) {
+            focusManager.moveFocus(focusDirection)
+        }
     }
 }
 
 /**
  * Custom scroll modifier extension which ties together focus listening and relocation requesting
  */
+@SuppressLint("UnnecessaryComposedModifier")
 @ExperimentalComposeUiApi
-private fun Modifier.scrollToOnFocus(scope: CoroutineScope, requester: RelocationRequester): Modifier =
-    relocationRequester(requester)
-    .onFocusEvent {
+private fun Modifier.scrollToOnFocus() = composed {
+    val scope = rememberCoroutineScope()
+    val requester = remember { RelocationRequester() }
+
+    relocationRequester(requester).onFocusEvent {
         if (it.isFocused) {
-            // Wait 250ms for the keyboard to animate in, such that the view
-            // resizes and [bringIntoView] scrolls down entirely.
             scope.launch {
                 delay(250)
                 requester.bringIntoView()
             }
         }
     }
+}
+
+@SuppressLint("UnnecessaryComposedModifier")
+private fun Modifier.focusOnEntry() = composed {
+    val imeInsets = LocalWindowInsets.current.ime
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(true) {
+        if (imeInsets.isVisible) {
+            focusRequester.requestFocus()
+        }
+    }
+
+    focusRequester(focusRequester = focusRequester)
+}
 
 
 // NOTE: This would be more performant if it was using [items], however due to a current
@@ -106,29 +106,27 @@ private fun Modifier.scrollToOnFocus(scope: CoroutineScope, requester: Relocatio
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalStdlibApi::class)
 @Composable
 private fun TodoListColumn(
-    scrollableState: ScrollState,
     lists: List<TodoList>,
     dispatch: (Any) -> Any,
 ) {
-    val scope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
 
-    Column(modifier = Modifier.verticalScroll(scrollableState)) {
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
         for (list in lists) {
-            val titleRequester = remember { RelocationRequester() }
             ListTitle(
                 title = list.title,
                 onValueChange = { dispatch(Action.UpdateListTitle(list, it)) },
                 onDoneAction = { dispatch(Action.AddTodo(list)) },
                 onDelete = { dispatch(Action.DeleteList(list)) },
-                modifier = Modifier.scrollToOnFocus(scope, titleRequester)
+                modifier = Modifier
+                    .scrollToOnFocus()
+                    .focusOnEntry()
             )
 
             for (item in list.items) {
-                val itemRequester = remember { RelocationRequester() }
-
                 TodoRow(
                     text = item.text,
                     isDone = item.isDone,
@@ -136,7 +134,9 @@ private fun TodoListColumn(
                     onTodoCheckedChange = { dispatch(Action.UpdateTodoDone(item, it)) },
                     onImeAction = { dispatch(Action.AddTodoAfter(item)) },
                     onDelete = { dispatch(Action.DeleteTodo(item)) },
-                    modifier = Modifier.scrollToOnFocus(scope, itemRequester)
+                    modifier = Modifier
+                        .scrollToOnFocus()
+                        .focusOnEntry()
                 )
             }
         }
