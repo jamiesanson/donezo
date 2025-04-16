3554ed879883f5e6fb05d2f43bc97ee4badc9228
Jamie Sanson <jamie@sanson.nz>
2025-04-16 21:48:06 +0100

    Replace non-automirrored icon with mirrored one

--- android/src/main/java/dev/sanson/donezo/screen/sync/SyncSettingsScreen.kt
+++ android/src/main/java/dev/sanson/donezo/screen/sync/SyncSettingsScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,7 +33,7 @@ fun SyncSettingsScreen(state: AppState) {
                             dispatch(Action.Navigation.Back)
                         },
                     ) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back to Lists")
+                        Icon(Icons.AutoMirrored.Default.ArrowBack, contentDescription = "Back to Lists")
                     }
                 },
             )
